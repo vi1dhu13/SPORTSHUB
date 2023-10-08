b9eecf83431c011f14924c1aa38a985c22d8047f
vi1dhu13 <77088289+vi1dhu13@users.noreply.github.com>
2023-10-08 20:01:23 +0530

    ko

--- TEST!/SportsHub/Members/views.py
+++
@@ -1,7 +0,0 @@
-# Members/views.py
-from django.shortcuts import render
-
-def landing_page(request):
-    return render(request, 'landing.html')
-
-# Create your views here.
--- tt/SportsHub/SportsHubApp/urls.py
+++ tt/SportsHub/SportsHubApp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.index, name='index'),
     path('services/', sports_center_list, name='services'),
     path('add-center/', add_center, name='add_center'),
-    # path('paymenthandler/', views.paymenthandler, name='paymenthandler'),
+  
     path('sports-centers/', SportsCenterListView.as_view(), name='sports_center_list'),
 
     path('sports-center/<int:sports_center_id>/select-slot/', views.select_slot, name='select_slot'),
