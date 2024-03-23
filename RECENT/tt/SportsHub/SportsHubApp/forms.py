be2d736a6b38f461d0134077d31318788d6e341d
vi1dhu13 <77088289+vi1dhu13@users.noreply.github.com>
2024-03-23 11:42:39 +0530

    updated

--- RECENT/tt/SportsHub/SportsHubApp/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-
-from django import forms
-from .models import InventoryItem
-
-class InventoryItemForm(forms.ModelForm):
-    class Meta:
-        model = InventoryItem
-        fields = ['name', 'quantity', 'image']
