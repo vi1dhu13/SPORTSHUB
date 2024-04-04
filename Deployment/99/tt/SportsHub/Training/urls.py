83ebd1416b1477d40354b67bc7027f2e493c0394
vi1dhu13 <77088289+vi1dhu13@users.noreply.github.com>
2024-04-04 22:26:45 +0530

    Update urls.py

--- Deployment/99/tt/SportsHub/Training/urls.py
+++ Deployment/99/tt/SportsHub/Training/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # ... Your other URL patterns ...
     path('choose_trainer/', views.choose_trainer, name='choose_trainer'),
     path('connection_list/', views.connection_list, name='connection_list'),
-    path('video_pose/', views.video_pose, name='video_pose'),
+
     path('render-my-template/', views.render_my_template, name='render_my_template'),
     path('workout_routines/', views.workout_routines, name='workout_routines'),
     path('create_routine/', views.create_routine, name='create_routine'),
